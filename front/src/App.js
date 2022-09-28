5ea1d35a0f67a2ab093ce66073c0481379377178
Aurelie Leconte <aurelieleconte.al@gmail.com>
2022-09-29 01:19:03 +0200

    micro test app

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import React from 'react'
+import axios from 'axios'
 
 function App() {
   const [newuser, setNewuser] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () =>{
+    axios.get('http://localhost:3000/user')
+      .then(function(response){
+        console.log(response);
+        setUsers(response.data)
+      })
+    .catch(function(error){console.log(error);});
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+
+  const sendUser = () => {  
+    axios.post('http://localhost:3000/user', { name: newuser }, /*{withCredentials:true} marche sans*/)
+      .then(function (response) { console.log(response); })
+      .catch(function (error) { console.log(error); });
+      getUsers();
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        
+        <div>
           <input type="text" value={newuser} onChange={(e) => setNewuser(e.target.value)}/>
-         {/* <button onClick={}>add user</button>*/}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button onClick={sendUser}>add user</button>
+          <h1>Users: </h1>
+          {users.map(user => (
+            <li key={user.name}>{user.name}</li>   
+          ))}
+        </div>
       </header>
     </div>
   );
